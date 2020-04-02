da645ae51dc893927c464f6ea86b2c94e2e688a7
ardianozi18 <oziardian@gmail.com>
2020-04-02 14:43:36 +0700

    Test 2 PHP

---
+++ PHP/Test2PHP.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Percobaan PHP</h1>
+
+<?php
+$testkalimat = "Halo Dunia!";
+$testangka = 12;
+$testlain = 3.5;
+
+echo $testkalimat;
+echo "<br>";
+echo $testangka;
+echo "<br>";
+echo $testlain;
+?> 
+
+</body>
+</html>
