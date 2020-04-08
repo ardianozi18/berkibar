2bf5c2aef7c769c55e9be7c0e7d24fd6606c2ff2
ardianozi18 <oziardian@gmail.com>
2020-04-08 20:45:10 +0700

    Test 6 PHP Functions

---
+++ PHP/Test6PHPFunctions.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function familyName($fname) {
+    echo "$fname Sunaryo.<br>";
+}
+
+familyName("Arysta");
+familyName("Arfian");
+familyName("Ardian");
+?>
+
+</body>
+</html>
