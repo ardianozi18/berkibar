d4a4d58049f962a7036413564f9cd2a039127fa5
ardianozi18 <oziardian@gmail.com>
2020-04-13 14:54:10 +0700

    Test PHP Include 2

---
+++ PHP/Test11PHPInclude.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<div class="testmenu">
+<?php include 'src/testmenu.php';?>
+</div>
+
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+
+</body>
+</html>
