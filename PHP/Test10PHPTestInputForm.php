912c20e73238826832db957d6c9cfeae07ab1894
ardianozi18 <oziardian@gmail.com>
2020-04-13 14:23:32 +0700

    Test PHP - Keep The Values in The Form 2

--- PHP/Test10PHPTestInputForm.php
+++ PHP/Test10PHPTestInputForm.php
@@ -78,9 +78,9 @@ Name: <input type="text" name="name" value="<?php echo $name;?>">
   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
   <br><br>
   Gender:
-  <input type="radio" name="gender" value="female">Female
-  <input type="radio" name="gender" value="male">Male
-  <input type="radio" name="gender" value="other">Other
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
   <span class="error">* <?php echo $genderErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
