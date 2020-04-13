f34c44d48b8346d27c8fefb42faf0f15399f2261
ardianozi18 <oziardian@gmail.com>
2020-04-13 22:45:04 +0700

    Test Write File

--- PHP/testfile.php
+++ PHP/testfile.php
@@ -1,3 +1,8 @@
 <?php
-$myfile = fopen("testfile.txt", "w")
+$myfile = fopen("testfile.txt", "w") or die("Unable to open file!");
+$txt = "Ardian Fauzi\n";
+fwrite($myfile, $txt);
+$txt = "Bella Praviyanti\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 ?>
